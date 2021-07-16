039443b9078586190ce7c3c65ac9fd8fd2a92b92
phamlam24 <ptlam61004@gmail.com>
2021-07-16 07:30:24 +0700

    fix document.write bug

--- 01/index.js
+++ 01/index.js
@@ -1,3 +1,8 @@
+document.write=function(s){
+    var scripts = document.getElementsByTagName('script');
+    var lastScript = scripts[scripts.length-1];
+    lastScript.insertAdjacentHTML("beforebegin", s);
+}
 // CODE ở trong này:
 document.getElementById("form").addEventListener("submit",(e)=>{
     // KHÔNG XÓA DÒNG NÀY
@@ -8,7 +13,5 @@ document.getElementById("form").addEventListener("submit",(e)=>{
 
     // ĐỀ BÀI: Input 1 số, output số gấp đôi số đó.
     // CODE TỪ ĐÂY
-
-
 
 })
